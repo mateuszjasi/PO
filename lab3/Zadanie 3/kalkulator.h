0a7dfc8ea40dab54c833a583b1dc8211d8db8b42
Mateusz Jasiński <54881442+mateuszjasi@users.noreply.github.com>
2023-04-13 23:12:17 +0200

    Add files via upload

---
+++ lab3/Zadanie 3/kalkulator.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+#include <string>
+#include <vector>
+
+class kalkulator
+{
+public:
+	kalkulator(std::string& dzialanie);
+	std::string dzialanie, liczba;
+	std::vector <double> dane;
+	std::vector <char> znaki;
+	int n = 0;
+	double wynik;
+	void deszyfr();
+	void licz();
+	void dodaj(int x);
+	void odejmij(int x);
+	void podziel(int x);
+	void pomnoz(int x);
+	void potega(int x);
+	void modulo(int x);
+	void procent(int x);
+	void pierwiastek(int x);
+};
---
+++ lab3/Zadanie 3/main.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <string>
+#include "kalkulator.h"
+
+int main()
+{
+	std::string dzialanie;
+	std::cout << "Podaj dzialanie: ";
+	//zeby podac liczbe ujemna nalezy wpisac ja ze znakiem '-' bezposredniu po znaku dzialania np 2*-2 = -4 lub 2--2 = 4
+	std::cin >> dzialanie;
+	kalkulator cal(dzialanie);
+	std::cout << "Wynik = " << cal.wynik;
+}
